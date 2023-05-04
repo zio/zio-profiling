60fe5e2868caaf1d4754bbd2760842dc32307012
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-05-04 05:24:13 +0200

    Update scala-collection-compat to 2.10.0 (#193)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,5 @@
 object Dependencies {
-  val collectionCompatVersion = "2.8.1"
+  val collectionCompatVersion = "2.10.0"
   val jmhVersion              = "1.36"
   val organizeImportsVersion  = "0.6.0"
   val silencerVersion         = "1.7.12"
