2f623a77209c70a930178292813d4bfd2487b5b7
Scala Steward <scala_steward@virtuslab.com>
2023-08-26 13:08:10 +0000

    Update zio, zio-streams, zio-test, ... to 2.0.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,5 +3,5 @@ object Dependencies {
   val jmhVersion              = "1.36"
   val organizeImportsVersion  = "0.6.0"
   val silencerVersion         = "1.7.12"
-  val zioVersion              = "2.0.14"
+  val zioVersion              = "2.0.16"
 }
