a0d938ac57842bd398dde6a5ae36135387e9ca29
Scala Steward <scala_steward@virtuslab.com>
2024-01-15 06:26:31 +0000

    Update jmh-core to 1.37

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,6 @@
 object Dependencies {
   val collectionCompatVersion = "2.11.0"
-  val jmhVersion              = "1.36"
+  val jmhVersion              = "1.37"
   val organizeImportsVersion  = "0.6.0"
   val silencerVersion         = "1.7.14"
   val zioVersion              = "2.0.14"
