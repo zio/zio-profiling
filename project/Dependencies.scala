280ad79952f0c8fa2c788c25ca7559963d04e235
Scala Steward <scala_steward@virtuslab.com>
2024-04-16 01:49:42 +0000

    Update scala-collection-compat to 2.12.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,5 @@
 object Dependencies {
-  val collectionCompatVersion = "2.11.0"
+  val collectionCompatVersion = "2.12.0"
   val jmhVersion              = "1.37"
   val zioVersion              = "2.0.22"
 }
